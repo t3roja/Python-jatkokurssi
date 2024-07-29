5c72e7a0894ad3a0e7657fe40921f9fba158c513
t3roja <t3roja@students.oamk.fi>
2024-07-29 18:26:10 +0300

    teht6

--- assignments/M06_threads/src/my_code.py
+++ assignments/M06_threads/src/my_code.py
@@ -1,35 +1,36 @@
-import threading
 import concurrent.futures as cf
 import time
 
-#heavy_computing for test purposes!
-#You may modify the function if necessary
-if __name__ == "__main__":
-    def heavy_computing(idx):
-        print('->heavy_computing('+str(idx)+')')
-        time.sleep(3)
-        print('<-heavy_computing('+str(idx)+')')
-        return idx, idx*idx
+# Heavy computing for test purposes
+def heavy_computing(idx):
+    print('->heavy_computing(' + str(idx) + ')')
+    time.sleep(3)
+    print('<-heavy_computing(' + str(idx) + ')')
+    return idx, idx * idx
 
 def start_threads(f, N):
-    thList = []
-    for i in range(N):
-        th=threading.Thread(target=f, args=(i,))
-        th.start()
-        thList.append(th)
-    return thList
+    futures = []
+    with cf.ThreadPoolExecutor() as executor:
+        for i in range(N):
+            future = executor.submit(f, i)
+            futures.append(future)
+    return futures
 
 def wait_threads(th_list):
-    for th in th_list:
-        th.join()
+    results = []
+    for future in cf.as_completed(th_list):
+        result = future.result()
+        results.append(result)
+    results.sort(key=lambda x: x[0])  # Sort results by idx to ensure the order
+    return [res for _, res in results]
 
-#Test software under this if        
+# Test software under this if        
 if __name__ == "__main__":
-    N=10
+    N = 10
 
     print('None started')
-    th_list=start_threads(heavy_computing, N)
+    th_list = start_threads(heavy_computing, N)
     print('Wait...')
-    ret=wait_threads(th_list)
+    ret = wait_threads(th_list)
     print('All futures completed')
     print(ret)
